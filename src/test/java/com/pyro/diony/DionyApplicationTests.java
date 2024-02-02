1d814a7673d3f294fdecb64937c1e8ab0848a462
yeonjy <yeonjypaper@gmail.com>
2024-02-02 10:40:14 +0900

    feat: spring 초기 세팅

---
+++ src/test/java/com/pyro/diony/DionyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyro.diony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DionyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
