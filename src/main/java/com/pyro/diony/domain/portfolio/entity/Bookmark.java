ce012a31df177f7865570913e667caa42828c04b
yeonjy <yeonjypaper@gmail.com>
2024-02-07 16:36:28 +0900

    feat: Bookmark entity & repository

---
+++ src/main/java/com/pyro/diony/domain/portfolio/entity/Bookmark.java
@@ -0,0 +1,32 @@
+package com.pyro.diony.domain.portfolio.entity;
+
+import com.pyro.diony.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    @Builder
+    public Bookmark(Member member, Portfolio portfolio) {
+        this.member = member;
+        this.portfolio = portfolio;
+    }
+}
